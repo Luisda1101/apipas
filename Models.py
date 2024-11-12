94c64b1d909f4d6e55ee8b459055a820c3961f6e
Luisda1101 <lvergarasolano62@correo.unicordoba.edu.co>
2024-11-12 00:30:28 -0500

    cambios

--- Models.py
+++ Models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -18,25 +17,4 @@ class Barbers(db.Model):
             "name": self.name,
             "service": self.service,
             "service_value": self.service_value
-=======
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Barbers(db.Model):
-    rowid = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), unique=True, nullable=False)
-    service = db.Column(db.String(200))
-    service_value = db.Column(db.Float, nullable=False)
-    
-    def __str__(self):
-        return f'Barbero: {self.name} - Servicio: {self.service} - Valor del servicio: {self.service_value}'
-
-    def serialize(self):
-        return {
-            "rowid": self.rowid,
-            "name": self.name,
-            "service": self.service,
-            "service_value": self.service_value
->>>>>>> 98b0007e253dd00035fa8e5021a15bfba88cc689
         }
