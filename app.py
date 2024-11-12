c0dec70e9c843a0159446372e1d8a4a4fe6dd86f
Luisda1101 <lvergarasolano62@correo.unicordoba.edu.co>
2024-11-12 00:29:53 -0500

    s

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, jsonify, request, render_template
 from Models import db, Barbers
 from logging import exception
@@ -66,73 +65,4 @@ def get_barbersForMoreVariables():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-=======
-from flask import Flask, jsonify, request, render_template
-from Models import db, Barbers
-from logging import exception
-import os
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:\\Users\\luisd\\OneDrive\\Escritorio\\Programación\\Proyecto PAS (Pagina web)\\ApiPAS\\database\\barbershop.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
-
-#Aquí empiezan las rutas
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/api/barbers", methods=["GET"])
-def get_barbers():
-    try:
-        barbers = Barbers.query.all()
-        toReturn = [barber.serialize() for barber in barbers]
-        return jsonify(toReturn), 200
-    except Exception:
-        exception("[SERVER]: Error ->")
-        return jsonify({"msg": "Ha ocurrido un error"}), 500
-
-@app.route("/api/barber", methods=["GET"])
-def get_barberByName():
-    try:
-        nameBarber = request.args["name"]
-        barber = Barbers.query.filter_by(name=nameBarber).first()
-        if not barber:
-            return jsonify({"msg": "Este barbero no existe"}), 200
-        else:
-            return  jsonify(barber.serialize()), 200
-    except Exception:
-        exception("[SERVER]: Error ->")
-        return jsonify({"msg": "Ha ocurrido un error"}), 500
-
-@app.route("/api/findbarber", methods=["GET"])
-def get_barbersForMoreVariables():
-    try:
-        fields = {}
-        if "name" in request.args:
-            fields["name"] = request.args["name"]
-        
-        if "service" in request.args:
-            fields["service"] = request.args["service"]
-            
-        if "service_value" in request.args:
-            fields["service_value"] = request.args["service_value"]
-
-        barber = Barbers.query.filter_by(**fields).first()
-        
-        if not barber:
-            return jsonify({"msg": "Este barbero no existe"}), 200
-        else:
-            return  jsonify(barber.serialize()), 200
-    except Exception:
-        exception("[SERVER]: Error ->")
-        return jsonify({"msg": "Ha ocurrido un error"}), 500
-
-#@app.route("/api/addbarber", methods=["POST"])
-#hola como estas
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
->>>>>>> 98b0007e253dd00035fa8e5021a15bfba88cc689
     #app.run(debug=True, port=5000)
--- database/seeder.py
+++ database/seeder.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sqlite3 as sql
 
 
@@ -33,40 +32,4 @@ def addValues():
 
 if __name__ == "__main__":
     createDB()
-=======
-import sqlite3 as sql
-
-
-DB_PATH = "C:\\Users\\luisd\\OneDrive\\Escritorio\\Programación\\Proyecto PAS (Pagina web)\\ApiPAS\\database\\barbershop.db"
-
-
-def createDB():
-    conn = sql.connect(DB_PATH)
-    cursor = conn.cursor()
-    cursor.execute("""CREATE TABLE streamers (
-        name String,
-        service String,
-        service_value float
-    )""")
-    conn.commit()
-    conn.close()
-
-
-def addValues():
-    conn = sql.connect(DB_PATH)
-    cursor = conn.cursor()
-    data = [
-        ("alexelcapo", 10000, 800000),
-        ("ibai", 25000, 7000000),
-        ("elxokas", 10000, 1000000),
-        ("auronplay", 20000, 8000000),
-        ("cristinini", 5500, 3000000)
-    ]
-    cursor.executemany("""INSERT INTO streamers VALUES (?, ?, ?)""", data)
-    conn.commit()
-    conn.close()
-
-if __name__ == "__main__":
-    createDB()
->>>>>>> 98b0007e253dd00035fa8e5021a15bfba88cc689
     addValues()
